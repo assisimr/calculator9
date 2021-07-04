bd70c5d22894fd9a6e7a14c036e3e4c44c8098ff
simran <ska7@njit.edu>
2021-07-04 12:17:16 -0400

    Feature: subtraction result

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,8 +1,13 @@
+from CsvReader import CsvReader
+
 def addition(a, b):
     return a + b
 
 def subtraction(a, b):
-    return a - b
+    a = int(a)
+    b = int(b)
+    c = a - b
+    return c
 
 class Calculator:
     result = 0
@@ -17,3 +22,11 @@ class Calculator:
     def subtract(self, a, b):
         self.result = subtraction(a, b)
         return self.result
+
+class CSVStats(Calculator):
+    data = []
+
+    def __init__(self, data_file):
+        super().__init__()
+        self.data = CsvReader(data_file)
+        pass
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,26 +1,32 @@
 import unittest
+
 from Calculator import Calculator
+from CsvReader import CsvReader
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
-    def test_results_property_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.result, 0)
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+
 
     def test_add_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(2,2), 4)
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(self.calculator.add(2,2), 4)
+        self.assertEqual(self.calculator.result, 4)
 
     def test_subtract_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.subtract(2,2), 0)
-        self.assertEqual(calculator.result, 0)
+        test_data = CsvReader('/src/subtraction.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_results_property_calculator(self):
+        self.assertEqual(self.calculator.result, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
